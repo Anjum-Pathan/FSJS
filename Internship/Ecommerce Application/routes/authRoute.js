c98bfbc05dab45b0eb4427876932cd71f580855a
anjumpathan <anjumpathan966@gmail.com>
2023-07-06 23:39:41 +0530

    completed

--- Internship/Ecommerce Application/routes/authRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { createUser } = require("../controller/userCtrl");
-const route = express.Router();
-
-route.post("/register", createUser);
-module.exports = route;
